edfe0e1e3a687c0e48fc473f42c71f0f816bcc08
bertoldosi <matheus.bertoldosi@gmail.com>
2022-07-07 22:36:23 -0300

    feat: Implemetando estrutura basica dos testes

---
+++ src/components/common/BreadCrumbs/BreadCrumbs.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import BreadCrumbs from ".";
+
+describe("first", () => {
+  test("should first", () => {
+    render(<BreadCrumbs />);
+  });
+});
